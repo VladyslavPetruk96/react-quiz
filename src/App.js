95ac035e217290fde64ff281473c821a123f3c30
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-02-27 14:47:54 +0200

    create fake api for fetching questiong & i json server & add reducer in app for fetch data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import DateCounter from './DateCounter';
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'active', 'ready', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      return new Error('Action unknown');
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/questions')
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(error => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
